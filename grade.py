22f274727b8dbbb2285fe796526aed15854a9836
Kormation <145355933+Kormation@users.noreply.github.com>
2023-12-08 09:29:27 -0500

    started assignment g
    n/a

--- grade.py
+++ grade.py
@@ -1,3 +1,27 @@
-# Programmer: 
-# Description: 
+# Programmer : Alexander Walker
+# Description : This program asks for a percent grade, if it's invalid the user will be given an error message
+#and will e prompted to try again
 
+grade = int(input("Please enter a grade % : "))
+
+while (grade < 0) or (grade > 100):
+    print("Invalid grade, must be between 0, 100")
+    grade = int(input("Please enter a grade % : "))
+    
+if (grade >= 80):
+    print("Grade : A")
+    
+elif (grade >= 70):
+    print("Grade : B")
+    
+elif (grade >= 60):
+    print("Grade : C")
+    
+elif (grade >= 50):
+    print("Grade : D")
+    
+else:
+    print("Grade : F")
+
+
+    
